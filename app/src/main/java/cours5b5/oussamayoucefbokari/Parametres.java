2bdf67d09d7ea3480370e8aa51f9bdc0210d7d79
Oussama Youcef Bokari <youcef-pino@hotmail.fr>
2018-08-23 15:13:17 -0400

    Le tout premier commit

---
+++ app/src/main/java/cours5b5/oussamayoucefbokari/Parametres.java
@@ -0,0 +1,13 @@
+package cours5b5.oussamayoucefbokari;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Parametres extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parametres);
+    }
+}
