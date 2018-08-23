9fe7c1135fb9883cd8056fd698ac39ab0ae50430
Oussama Youcef Bokari <youcef-pino@hotmail.fr>
2018-08-23 15:38:26 -0400

    Fin de l'atelier 1

--- app/src/main/java/cours5b5/oussamayoucefbokari/Parametres.java
+++ app/src/main/java/cours5b5/oussamayoucefbokari/Parametres.java
@@ -2,6 +2,7 @@ package cours5b5.oussamayoucefbokari;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class Parametres extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class Parametres extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parametres);
+        Log.d("MonEtiquette","Bonjour");
     }
 }
